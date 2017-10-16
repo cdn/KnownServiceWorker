308da1e4187493d7d3a789d809d64657b89a2664
C <cn@chrs.nl>
2017-10-16 10:54:16 +0000

    adactio–like js–generated markup

--- IdnoPlugins/Robot/templates/default/robot/pages/offline.tpl.php
+++ IdnoPlugins/Robot/templates/default/robot/pages/offline.tpl.php
@@ -1,11 +1,20 @@
 <!-- à la adactio.com/offline -->
-<h1>Something is Offline</h1>
-
-<p>There’s an issue with connectivity…</p>
-
-<p>Here’s something to look at:</p>
-
-<div id="history">
+<div class="container result-404">
+    <div class="row" style="margin-bottom: 2em; margin-top: 6em">
+        <div class="col-md-offset-1 col-md-5">
+            <h1 class="p-name" style="margin-bottom: 2em;">
+                Offline.
+            </h1>
+            <p>Whatever you were looking for, it's not here. It might have been moved, deleted, or it doesn't exist. Or the robots ate it. That's always a possibility too.</p>
+            <p>Maybe you'll find something interesting if you head back to the <a href="<?=\Idno\Core\Idno::site()->config()->getDisplayURL()?>"><?=\Idno\Core\Idno::site()->config()->title?> homepage</a>.
+            </p>
+            <div id="history">
+            </div>
+        </div>
+        <div class="col-md-5">
+	        <img src="<?=\Idno\Core\Idno::site()->config()->getDisplayURL()?>gfx/robots/aleph_404.png" alt="Robot with a missing sign">
+        </div>
+    </div>
 </div>
 
 <script>
