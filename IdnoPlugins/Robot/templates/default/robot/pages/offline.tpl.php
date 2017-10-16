9a95b5a1f4f7cc49fadeb4f9d26b9a3c58d027e7
C <cn@chrs.nl>
2017-10-16 10:59:16 +0000

    alternative js–generated markup; needs better styling

--- IdnoPlugins/Robot/templates/default/robot/pages/offline.tpl.php
+++ IdnoPlugins/Robot/templates/default/robot/pages/offline.tpl.php
@@ -5,11 +5,12 @@
             <h1 class="p-name" style="margin-bottom: 2em;">
                 Offline.
             </h1>
-            <p>Whatever you were looking for, it's not here. It might have been moved, deleted, or it doesn't exist. Or the robots ate it. That's always a possibility too.</p>
+            <p>Whatever you were looking for, it's not here at the moment. It might have been moved, deleted, or it doesn't exist. Or the robots ate it. That's always a possibility too.</p>
             <p>Maybe you'll find something interesting if you head back to the <a href="<?=\Idno\Core\Idno::site()->config()->getDisplayURL()?>"><?=\Idno\Core\Idno::site()->config()->title?> homepage</a>.
             </p>
-            <div id="history">
-            </div>
+            <p>There may be some other things to look at:</a>
+            <dl id="history">
+            </dl>
         </div>
         <div class="col-md-5">
 	        <img src="<?=\Idno\Core\Idno::site()->config()->getDisplayURL()?>gfx/robots/aleph_404.png" alt="Robot with a missing sign">
@@ -37,9 +38,9 @@ caches.open('pages')
         browsingHistory.forEach( data => {
           if(data.url.indexOf('offline') == -1)
             markup += `
-<h2><a href="${ data.url }">${ data.title }</a></h2>
-<p>${ data.description }</p>
-<!-- <p class="meta">${ data.published }</p> -->
+<dt><a href="${ data.url }">${ data.title }</a></dt>
+<dd>${ data.description }</dd>
+<!-- <p class="meta">${ data.published }</p></dd> -->
 `;
         });
         let container = document.getElementById('history');
