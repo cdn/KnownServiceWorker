8545f3c4a30a178202c2d26eaa40a943053d7ee3
C <cn@chrs.nl>
2017-10-18 13:48:42 +0000

    Moving localStorage input to another template to aid specificity

--- IdnoPlugins/Robot/Main.php
+++ IdnoPlugins/Robot/Main.php
@@ -10,11 +10,12 @@ namespace IdnoPlugins\Robot {
 	    \Idno\Core\Idno::site()->template()->extendTemplate('shell/footer', 'robot/shell/footer');
 	    // Service worker javascript file to be copied to site root directory
 
-            // Need to find the slot for this post–content for permalink URLs
-            //\Idno\Core\Idno::site()->template()->extendTemplate('entity/shell|wrapper', 'robot/shell/wrapper');
+            // Need to find the slot for this post–content for localStorage of metadata for permalink URLs
+            //\Idno\Core\Idno::site()->template()->extendTemplate('content/end', 'robot/content/end');
+            //\Idno\Core\Idno::site()->template()->extendTemplate('content/syndication/links', 'robot/content/end');
+            // Unsuitable given repeated in a list of entities
 
-            // Augment 404 as a quick /offline [not post-content]
-            //\Idno\Core\Idno::site()->template()->extendTemplate('pages/404', 'robot/pages/offline');
+            \Idno\Core\Idno::site()->template()->extendTemplate('entity/annotations/comment/main', 'robot/entity/anno_comm_main');
 
             // The full page route
             \Idno\Core\Idno::site()->addPageHandler('/offline/?', '\IdnoPlugins\Robot\Pages\Offline');
