44cbb45fecc07b3a0725ce4ceec2430570121933
C <cn@chrs.nl>
2017-10-18 14:06:45 +0000

    Remove redundant (global) localStorage portion

--- IdnoPlugins/Robot/templates/default/robot/shell/footer.tpl.php
+++ IdnoPlugins/Robot/templates/default/robot/shell/footer.tpl.php
@@ -1,28 +1,2 @@
 
-<script>
-if (navigator.serviceWorker && navigator.serviceWorker.controller) {
-  window.addEventListener('load', function() {
-var times = document.getElementsByTagName('time');
-if(times.length == 1) {
-var datetime = times[0].getAttribute("datetime");
-var dt = new Date(datetime);
-var description = document.querySelector("meta[name='description']").getAttribute("content");
-var timestamp = dt.getTime();
-var title = document.querySelector("title").innerText;
-description = (description != title) ? description : '';
-var data = {
-  "title": title,
-  "description": description,
-  "published": datetime,
-  "time_tip":  times[0].getAttribute("title"),
-  "timestamp": timestamp
-};
-    localStorage.setItem(
-      window.location.href,
-      JSON.stringify(data)
-    );
-}
-  });
-}
-</script>
 <script src="<?= \Idno\Core\site()->config()->url; ?>IdnoPlugins/Robot/vendor/sw-loader.min.js"></script>
